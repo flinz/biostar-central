210b9abcc1e27edfaf099ffa985063cfbcb7f761
Alex Seeholzer <seeholzer@pool-226-33-zam038.wlan.kfa-juelich.de>
2014-01-27 15:52:55 +0100

    Forked from github.com/brainstorm. Modified to make the site run on local distro.

--- main/server/feeds.py
+++ main/server/feeds.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.sites.models import Site
 from datetime import datetime, timedelta
 
-SITE = Site.objects.get(id=settings.SITE_ID)
+#SITE = Site.objects.get(id=settings.SITE_ID)
 
 class LatestEntriesFeed(Feed):
     title = "NeuroStarss.org latest!"
